5e6b7aff758724e86a2b9f31a861e3c42b5056fa
Darren Le Minh Duc <leminhduc74@gmail.com>
2018-01-04 17:48:44 +0800

    Support both direct/indirect input modes

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -5,11 +5,15 @@ using UnityStandardAssets.Characters.ThirdPerson;
 [RequireComponent(typeof (ThirdPersonCharacter))]
 public class PlayerMovement : MonoBehaviour
 {
+
+
 	[SerializeField] float walkMoveStopRadius = 0.2f;
 
-    ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
-    CameraRaycaster cameraRaycaster;
-    Vector3 currentClickTarget;
+    private ThirdPersonCharacter m_Character;   // A reference to the ThirdPersonCharacter on the object
+    private CameraRaycaster cameraRaycaster;
+    private Vector3 currentClickTarget;
+	private bool isInDirectMode = false; // TODO consider making static later
+	private bool m_Jump;
         
     private void Start()
     {
@@ -18,15 +22,46 @@ public class PlayerMovement : MonoBehaviour
         currentClickTarget = transform.position;
     }
 
+	private void Update() {
+		if (!m_Jump)
+		{
+			m_Jump = Input.GetButtonDown("Jump");
+		}
+	}
+
     // Fixed update is called in sync with physics
     private void FixedUpdate()
     {
-        if (Input.GetMouseButton(0))
-		{
-			print("Cursor raycast hits layer: " + cameraRaycaster.layerHit);
+		if (Input.GetKeyDown (KeyCode.G)) { 
+			// G for gamepad. TODO allow player to map later
+			isInDirectMode = !isInDirectMode; // toggle mode
+		}
+		if (isInDirectMode) {
+			ProcessDirectMovement();
+		} else {
+			ProcessMouseMovement();
+
+		}
+    }
+
+	private void ProcessDirectMovement () {
+		// Evaluate movement vectors
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+		Vector3 m_CamForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+		Vector3 m_Move = v*m_CamForward + h * Camera.main.transform.right;
+		// Pass movement vector to chracter model
+		m_Character.Move(m_Move, false, m_Jump);
+		m_Jump = false;
+	}
+
+	private void ProcessMouseMovement ()
+	{
+		if (Input.GetMouseButton (0)) {
 			switch (cameraRaycaster.layerHit) {
 			case Layer.Walkable:
-				currentClickTarget = cameraRaycaster.hit.point;  // So not set in default case
+				currentClickTarget = cameraRaycaster.hit.point;
+				// So not set in default case
 				break;
 			case Layer.Enemy:
 				print ("NOT MOVING TO ENEMY");
@@ -34,14 +69,14 @@ public class PlayerMovement : MonoBehaviour
 			default:
 				// Stand still
 				return;
-			}	
-        }
+			}
+		}
 		// Handle movement outside
 		Vector3 playerToClickPoint = currentClickTarget - transform.position;
 		if (playerToClickPoint.magnitude < walkMoveStopRadius) {
 			playerToClickPoint = Vector3.zero;
 		}
 		m_Character.Move (playerToClickPoint, false, false);
-    }
+	}
 }
 
