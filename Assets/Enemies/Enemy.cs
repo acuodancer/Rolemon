8505fc5dc288c72ef3b3a666a39de589cc3d736f
Darren Le Minh Duc <leminhduc74@gmail.com>
2018-01-05 21:00:57 +0800

    Add prototype level with some assets and terrain texture

---
+++ Assets/Enemies/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	[SerializeField] float maxHealthPoint = 100f;
+	private float currentHealthPoint = 100f;
+
+	public float healthAsPercentage {
+		get {
+			return currentHealthPoint / maxHealthPoint;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
