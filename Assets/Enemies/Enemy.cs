56f6531d4364b8af56883840b7d5bb47f8dc6070
Darren Le Minh Duc <leminhduc74@gmail.com>
2018-01-05 22:49:56 +0800

    Add basic NavMesh and AI script for pursue and stop pursue player

--- Assets/Enemies/Enemy.cs
+++ Assets/Enemies/Enemy.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 public class Enemy : MonoBehaviour {
 
+	[SerializeField] float viewRadius = 2f;
+	[SerializeField] float pursueRadius = 3f;
 	[SerializeField] float maxHealthPoint = 100f;
 	private float currentHealthPoint = 100f;
+	private AICharacterControl aiCharacterControl = null;
+	private GameObject player= null;
 
 	public float healthAsPercentage {
 		get {
@@ -15,11 +20,21 @@ public class Enemy : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
+		player = GameObject.FindGameObjectWithTag ("Player");
+		aiCharacterControl = GetComponent<AICharacterControl> ();
 	}
 
 	// Update is called once per frame
 	void Update () {
-
+		Vector3 vecToPlayer = transform.position - player.transform.position;
+		if (vecToPlayer.magnitude <= viewRadius) {
+			aiCharacterControl.SetTarget(player.transform);
+		} else {
+			if (aiCharacterControl.target != null) {
+				if (vecToPlayer.magnitude > pursueRadius) {
+					aiCharacterControl.SetTarget (null);
+				}	
+			} 
+		}
 	}
 }
