62bde98be54ae8702b049d62a3d6d2b0f5c60d44
Darren Le Minh Duc <leminhduc74@gmail.com>
2018-01-09 19:37:10 +0800

    Add material to player Add projectile with collision detection script

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour, IDamageable {
 
 	[SerializeField] float maxHealthPoint = 100f;
 	private float currentHealthPoint = 100f;
@@ -12,6 +12,11 @@ public class Player : MonoBehaviour {
 			return currentHealthPoint / maxHealthPoint;
 		}
 	}
+
+    public void TakeDamage(float damage)
+    {
+        currentHealthPoint = Mathf.Clamp(currentHealthPoint, damage, 0);
+    }
 
 	// Use this for initialization
 	void Start () {
---
+++ Assets/Player/Projectile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("Projectile hit " + other.gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
